0ac4f1b6f5ec70ffd2bf358c6801751ed49ef276
NguyenTien99 <nguyenmtien0@gmail.com>
2022-07-20 20:38:26 +0700

    update

--- BaiTap4/main.js
+++ BaiTap4/main.js
@@ -4,7 +4,7 @@ document.getElementById("nutnhap").onclick = function(){
     var chieuRong = document.getElementById("chieurong").value * 1;
     //Xử lí
     var dienTich = chieuDai * chieuRong;
-    var chuVi = (chieuDai + chieuRong) / 2; 
+    var chuVi = (chieuDai + chieuRong) * 2; 
     var result = "<div>";
     result += "<p> Diện tích : " + dienTich + "</p>";
     result += "<p> Chu vi : " + chuVi + "</p>";
